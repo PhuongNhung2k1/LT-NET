f4df698fd1dcc2e4bf6f5ae867389d8b036ffa44
PhuongNhungPham <78036028+PhuongNhung2k1@users.noreply.github.com>
2021-12-24 09:03:49 +0700

    Add files via upload

---
+++ Room.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhamThiPhuongNhung_987_De02
+{
+    class Room
+    {
+        private string soPhong;
+        private int soCho;
+        public static int giaCoBan = 100;
+        public string SP
+        {
+            get { return soPhong; }
+            set { value = soPhong; }
+        }
+         public int SoCho
+        {
+            get { return soCho; }
+            set { value = SoCho; }
+        }
+        public Room() { }
+        public Room(string SP, int soChoNam) {
+            this.soPhong = SP;
+            this.soCho = soChoNam;
+        }
+        public virtual void Nhap()
+        {
+            Console.Write("Nhap vao ma so phong :");
+            soPhong = Console.ReadLine();
+            Console.Write("Nhap vao ma so luong giuong :");
+            soCho = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
